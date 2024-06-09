8d7632976cbc199cd8985e38d9fab0bd73f1545b
Luca Ilari <luca.ilari@gmail.com>
2024-06-09 10:53:40 +0200

    Test with API

--- app/censimento-parcheggi/censimento-parcheggi.jsx
+++ app/censimento-parcheggi/censimento-parcheggi.jsx
@@ -1,18 +1,84 @@
-import React from 'react';
-import { View, Text} from 'react-native';
-import { createClient } from "@supabase/supabase-js";
+import { Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Text, FlatList, ActivityIndicator, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, TextInput, View } from "react-native-web";
 
-const supabaseUrl = "https://yuyyujadubndgfpxauug.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1eXl1amFkdWJuZGdmcHhhdXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2NjA4MzUsImV4cCI6MjAzMzIzNjgzNX0.V8bBPKuA3fXB10LcA1inEJRDAv96y - ePQaNdpaKO0yo";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const API_SERVER_URL = "http://127.0.0.1:5000";
 
-const MyComponent = () => {
-    return (
-        <View>
-            <Text>Hello, React Native!</Text>
-        </View>
-    );
+const useFilms = () => {
+    const [loading, setLoading] = useState(true);
+    const [failed, setFailed] = useState(false);
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(API_SERVER_URL + "/get_films");
+                setFilms(await res.json());
+                setLoading(false);
+            } catch (error) {
+                setFailed(true);
+            }
+        })();
+    }, []);
+    return { loading, failed, films };
 };
 
+async function postTest(name) {
+    if (name) {
+        try {
+            const res = await fetch(API_SERVER_URL + "/add_film", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ film_name: name }),
+            });
+            const test = await res.json();
+        } catch (error) {}
+    }
+}
 
-export default MyComponent;
+const CensimentoParcheggi = () => {
+    const { loading, failed, films } = useFilms();
+    const [uInput, setUInput] = useState("");
+    const addFilm = () => {
+        postTest(uInput);
+    };
+
+    return (
+        <>
+            <Stack.Screen options={{ headerTitle: "Gestione Parcheggio" }} />
+            <SafeAreaView>
+                <View style={style.container}>
+                    <View>
+                        {!loading ? (
+                            <FlatList
+                                style={style.list}
+                                contentContainerStyle={{ gap: 8 }}
+                                data={films}
+                                renderItem={(film) => <Text>- {film?.item?.nome}</Text>}
+                            />
+                        ) : (
+                            <ActivityIndicator size="large" />
+                        )}
+                    </View>
+                    <View>
+                        <TextInput
+                            placeholder="Inserire nome del film"
+                            name="input"
+                            onChange={(e) => {
+                                setUInput(e.target.value);
+                            }}
+                        />
+                        <Button type="submit" title="SUBMIT" onPress={addFilm} />
+                    </View>
+                </View>
+            </SafeAreaView>
+        </>
+    );
+};
+const style = StyleSheet.create({
+    list: { gap: 2, marginBottom: "1vw" },
+    container: { flex: 0, alignItems: "center" },
+});
+export default CensimentoParcheggi;
