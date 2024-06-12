8995d48f3d830c4ac2286f3be98819810274236b
bendoz0 <147492032+bendoz0@users.noreply.github.com>
2024-06-12 08:45:45 +0200

    creata la pagina-1, manca la barra di avanzamento

--- app/flusso-pagamento/Pagina-1.tsx
+++ app/flusso-pagamento/Pagina-1.tsx
@@ -1,27 +1,53 @@
-import React from "react";
-import {TextInput, Text, View, StyleSheet} from "react-native"
+import React, {useState} from "react";
+import {TextInput, Text, StyleSheet, View, TouchableOpacity, Button} from "react-native";
+import {Dropdown, styles} from "@/app/flusso-pagamento/Components/ListaTarghe";
+import {router} from "expo-router";
+
 
 const firstPage = () => {
+    const handleSelect = (option: string) => {
+    };
     return (
-        <View>
-            <Text>Seleziona la targa dell'auto parcheggiata:</Text>
-            <View style={styles.container}>
-                <Text style={styles.text}>Inserisci una nuova targa:</Text>
-                <TextInput style={styles.text} placeholder="AA000AA"/>
-                <hr></hr>
-                <button>Prosegui</button>
-            </View>
+        <View style={styless.container}>
+            <Text style={styless.text}>Seleziona la targa dell'auto parcheggiata: </Text>
+            <Dropdown options={['AA 000 AA', 'AA 000 AB', 'AA 000 AC']} onSelect={handleSelect}/>
+            <Text style={[styless.text]}>Inserisci una nuova targa: </Text>
+            <TextInput style={[styles.dropdownButton, styles.dropdownButtonText, styless.textCenter]}
+                       placeholder="AA 000 AA"/>
+            <TouchableOpacity style={styless.nextBtn} onPress={() => router.push("/flusso-pagamento/Pagina-2" )}>
+                <Text style={{color: "white"}} >Prosegui</Text>
+            </TouchableOpacity>
         </View>
     )
 }
-
 export default firstPage
 
-const styles = StyleSheet.create({
+const styless = StyleSheet.create({
     container: {
-        backgroundColor: 'black',
+        top: 50,
+        alignItems: 'center',
+        gap: 16,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "grey",
+        color: "black",
+        borderRadius: 6
     },
     text: {
-        color: "white"
+        fontSize: 32,
+        color: "black"
+    },
+    nextBtn: {
+        top: 100,
+        padding: 12,
+        backgroundColor: '#155e85',
+        borderRadius: 6
+    },
+    textCenter: {
+        textAlign: "center",
+        fontSize: 20,
+        padding: 8,
+
     }
-})  
+})
