3331a3676953950fef9a11fa675c37628cd2db0a
DiegoBendinelliITS <diego.bendinelli@itsrizzoli.it>
2024-06-18 15:32:31 +0200

    migrazione tsx

--- app/gestione-segnalazioni/gestione-segnalazione.tsx
+++ app/gestione-segnalazioni/gestione-segnalazione.tsx
@@ -1,19 +1,14 @@
 import React from "react";
+import { router } from "expo-router"
 import {TextInput, Text, View, Button} from "react-native";
 
-const Test = () => {
-    return (
+export default function Test(){
+    return(
         <View>
-            <Text>Test</Text>
-            <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-            />
-            <Text></Text>
-            <Button
-                title="Press Me"
-            />
-        </View>
-    );
-}
 
-export default Test;
+      
+        <Button onPress={() => router.push("./segnalazioni-da-utente")} title={'SENDTO NEXT PAGE'}/>
+        </View>
+    )
+
+}
