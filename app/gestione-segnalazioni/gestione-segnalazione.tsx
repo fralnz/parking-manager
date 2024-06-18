51f9d79429f099dd958e79c754d63d80c5870bff
GiacomoGrieco <giacomo.grieco@itsrizzoli.it>
2024-06-18 15:31:13 +0200

    smas
    creazione primo file gestione segn

---
+++ app/gestione-segnalazioni/gestione-segnalazione.tsx
@@ -0,0 +1,13 @@
+import {Button, Text, View} from "react-native";
+import {router, Stack} from "expo-router"; //questa è la pagina principale !
+import React from "react";
+
+export default function Gestione() {
+  return (
+    <>
+    <View>
+        <Text>SMAS</Text>
+    </View>
+    </>
+  )
+}
