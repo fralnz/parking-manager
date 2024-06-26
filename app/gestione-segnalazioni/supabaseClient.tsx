223dfac8a5c19e98f8cf987711f99c9771c33d20
DiegoBendinelliITS <diego.bendinelli@itsrizzoli.it>
2024-06-26 10:34:26 +0200

    Non funziona un cazzo ma almeno faccio una copia in caso si possa usare per qualcosa che funzioni

--- app/gestione-segnalazioni/supabaseClient.tsx
+++ app/gestione-segnalazioni/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = ''; // Sostituisci con il tuo URL Supabase
-const supabaseKey = 'your-anon-key'; // Sostituisci con la tua chiave anonima
+const supabaseUrl = 'https://yuyyujadubndgfpxauug.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1eXl1amFkdWJuZGdmcHhhdXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2NjA4MzUsImV4cCI6MjAzMzIzNjgzNX0.V8bBPKuA3fXB10LcA1inEJRDAv96y-ePQaNdpaKO0yo";
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
